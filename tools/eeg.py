7877bf8b1086f3f4ce762bede6ed95b9fa1354cb
Matt Miller <millermatt090@gmail.com>
2023-12-07 20:26:23 -0800

    commit before pull

--- tools/eeg.py
+++ tools/eeg.py
@@ -8,11 +8,12 @@ import pickle
 import csv
 import re
 import time 
-#import gspread
+import gspread
 #from Matching import *
 #from app import *
 
-from tools.logging import logger   
+from tools.logging import logger
+
 
 """
 #doing all this a the "module level" in "Demo" server mode it will work fine :)
@@ -36,14 +37,26 @@ for i in names:
 def on_sensor_state_changed(sensor, state):
     logger.debug('Sensor {0} is {1}'.format(sensor.Name, state))
 
+data_to_pickle = []
 def on_brain_bit_signal_data_received(sensor, data):
+    global data_to_pickle
     logger.debug(data)
-    
-    #Comment out when using headband
-    #Get mock data to simulate using headband store into data 
+
     file = open('tools/MockBrainData1.pkl', 'r')
     data = file.readlines()
     file.close()
+
+    data_to_pickle.append(data)
+
+    print(data)
+
+
+    """
+    #Comment out when using headband
+    #Get mock data to simulate using headband store into data 
+    #file = open('tools/MockBrainData1.pkl', 'r')
+    #data = file.readlines()
+    #file.close()
     
    #Pickle testing: Send mock data to pickle file as if it were real data, pickle it then unpickle it/
     with open('BrainDataFile.pkl', 'wb') as file: 
@@ -58,8 +71,17 @@ def on_brain_bit_signal_data_received(sensor, data):
 
     #get_avgs(data)
 
-    
+with open('BrainDataFile.pkl', 'wb') as file: 
+        pickle.dump(data, file)
+        file.close()
 
+"""
+
+def pickle_brain_bit_data(data): 
+    with open('BrainDataFile.pkl', 'wb') as file: 
+        pickle.dump(data, file)
+        file.close()
+    
 
 logger.debug("Create Headband Scanner")
 gl_scanner = Scanner([SensorFamily.SensorLEBrainBit])
@@ -83,22 +105,11 @@ gl_scanner.sensorsChanged = sensorFound
 logger.debug("Start scan")
 gl_scanner.start()
 
-logger.debug("Stop Scan")
-gl_scanner.stop()
+#logger.debug("Stop Scan")
+#gl_scanner.stop()
 
 
 def get_head_band_sensor_object():
     return gl_sensor
 
-''''
-with open('output.csv', 'w', newline='') as file:
-            sys.stdout = file  # Redirect stdout to the file
-            if Sensor.is_supported_command(SensorCommand.CommandStartSignal):
-                Sensor.exec_command(SensorCommand.CommandStartSignal) #this line prints the data
-                print("Start signal")
-                sleep(5)
-                Sensor.exec_command(SensorCommand.CommandStopSignal)
-                print("Stop signal")
-            sys.stdout = sys.stdout
-'''
 
