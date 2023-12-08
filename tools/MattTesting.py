b5c8c54cb99dac09388e296a8271f869dbba6ad7
Matt Miller <millermatt090@gmail.com>
2023-12-07 20:43:27 -0800

    Fixed

--- tools/MattTesting.py
+++ tools/MattTesting.py
@@ -1,270 +1,163 @@
-from flask import Flask,render_template,request, redirect, url_for, g
-from flask_json import FlaskJSON, JsonError, json_response, as_json
-import jwt
-
-import sys
-import datetime
-import bcrypt
-import traceback
-import json #for file creation in .json
-import os #for directory specifiy
+#import gspread
+from parseAlgo import * 
+#from app import *
+import pickle
+#import json
 import psycopg2
+#from app import current_user
 
-from tools.eeg import get_head_band_sensor_object
-
-
-from db_con import get_db_instance, get_db
-
-from tools.token_required import token_required
-
-#used if you want to store your secrets in the aws valut
-#from tools.get_aws_secrets import get_secrets
-
-from tools.logging import logger
-
-#variables for database communication
 hostname = 'umamind-1.crh1scx9g148.us-east-1.rds.amazonaws.com'
 database = 'postgres'
 username = 'UmaMindGroup'
 pwd = 12132023
 port_id = 5432
-#conn = None
-#cur = None
+conn = None
+cur = None
 
-database_info = {
+#queiries 
+insertUser = 'INSERT INTO users (fname, lname, age, gender) VALUES (%s, %s, %s, %s)'
+insertAvgs = 'INSERT INTO data (o1, o2, t3, t4) VALUES (%s, %s, %s, %s)'
 
-}
-
-ERROR_MSG = "Ooops.. Didn't work!"
-global_array = []#an array that holds json/database profiles
-current_user = "no_user"#string value that holds first name to locate current user
-
-#Create our app
-app = Flask(__name__, template_folder = 'static')
-#add in flask json
-FlaskJSON(app)
-
-#print("This is on startup")
-
-
-
-#Function obtains all current profiles in from database to a global array
-def ar_profile():
-    conn = None
-    cur = None 
+#this function calculates averages from mock pickl file and stores the results to the data table in database
+def calcAndStoreAvg():
     try:
-        conn = psycopg2.connect(host = hostname, dbname = database, user = username, password = pwd, port = port_id)
+        conn = psycopg2.connect(host=hostname, dbname=database, user=username, password=pwd, port=port_id)
         print("connected")
         cur = conn.cursor()
 
-        get_profile_script = 'SELECT * FROM users'
-        cur.execute(get_profile_script)
-        #print(cur.fetchall())
-        rows = cur.fetchall()
+        #keep count of all of the variables
+        O1t = 0.0
+        O2t = 0.0
+        T3t = 0.0
+        T4t = 0.0
 
-        for row in rows:
-            global_array.append(row)
-            #print(row)
+        #row counter
+        count = 1
+
+        with open('BrainDataFile.pkl', "rb") as file: 
+            data = pickle.load(file)
+            #parse out all of the variables
+            O1 = (getO1(str(data)))
+            O2 = (getO2(str(data)))
+            T3 = (getT3(str(data)))
+            T4 = (getT4(str(data)))
+
+            #add variables as floats 
+            O1_length = len(O1)
+            O2_length = len(O2)
+            T3_length = len(T3)
+            T4_length = len(T4)
+
+            for i in range(O1_length): 
+                 O1t += float(O1[i])
     
-        #print(global_array[0])
-        #print(global_array[1])
-        #print(global_array[2])
+            for i in range(O2_length): 
+                O2t += float(O2[i])
 
+            for i in range(T3_length):     
+                T3t += float(T3[i])
+    
+            for i in range(T4_length):
+                T4t += float(T4[i])
+
+            #print current variables
+            #print("O1: "+O1 +" "+"O2: "+O2+ " "+"T3: "+T3+ " "+"T4: "+T4) 
+            count += 1 #update counter
+
+            file.close()
+
+        #find averages
+        O1t = O1t / O1_length
+        O2t = O2t / O2_length
+        T3t = T3t / T3_length
+        T4t = T4t / T4_length
+
+        #display averages
+        '''
+        print("averages are: ")
+        print("01: " + str(O1t) + " O2: " + str(O2t) + " T3: " + str(T3t) + " T4: " + str(T4t))
+        '''
+        #insertHardCode = (-0.107654321098765, -0.106543210987654, -0.095432109876543, -0.112345678901234)
+
+        #averagesInsert = (O1t,  O2t, T3t, T4t)  #used for when current user is new to the database
+        #averagesUpdate = (O1t,  O2t, T3t, T4t, current_userID)  #used for when current user is already in db
+
+        '''   
+        matchFound = False
+        getUserInfo_script = 'SELECT id FROM users'
+
+        cur.execute(getUserInfo_script)
+        user_ids = [row[0] for row in cur.fetchall()]
+
+        for id in user_ids:
+            if current_userID == id:
+                matchFound = True
+    
+        if matchFound == True:
+            cur.execute('UPDATE data SET o1 = %s, o2 = %s, t3 = %s, t4 = %s WHERE user_id = %s', averagesUpdate)
+            print("already in database, updating averages")
+        else:
+            cur.execute(insertUser, ("Bob", "Builder", 25, "male"))
+            cur.execute(insertAvgs, insertHardCode)
+            print("new to database, adding new averages to database")
+        ''' 
+        #store calculated averages into averages, send them to the data table using insertAvg query
+        averagesInsert = (O1t,  O2t, T3t, T4t)
+        cur.execute(insertAvgs, averagesInsert)
 
         conn.commit()
     except Exception as error:
         print(error)
-    finally:
+    finally: 
         if cur is not None:
             cur.close()
         if conn is not None:
             conn.close()
 
 
-def update_current_user(new_user):#function that updates current_user string
-    global current_user
-    current_user = new_user
-
-@app.route('/login', methods=['POST', 'GET'])
-def login_form():
-    #print("Login button")
-    ar_profile()#update array with current database data
-    count = len(global_array)
-    first = request.form['First Name']
-    last = request.form['Last Name']
-    
-    for i in range(count):
-        if first == global_array[i][1]: #1 is tuple for first name & i is array slot
-            if last == global_array[i][2]:
-                update_current_user(global_array[i])
-                return redirect('static/UmamindHome.html')
-            else:
-                i = i + 1
-        else:
-            i = i + 1#incriment
-    
-    #print(global_array[0])
-    #print(global_array[0][1])
-    #print("Profile not found")
-    return redirect('static/2nd Login.html')
-
-
-@app.route('/submit', methods=['POST'])#function that creates a .json file with user inputed first and last name and stores in profiles folder
-def submit_form():
-    if request.method == 'POST':
-        FirstName = request.form['fname']
-        LastName = request.form['lname']
-        Age = request.form['Age']
-        Gender = request.form['Gender']
-        '''
-        data = {#creates a .js object file that holds first and last name
-            'fname': FirstName,
-            'lname': LastName,
-            'Age': Age,
-            'Gender' : Gender
-
-        }
-        '''
-        #database update/communication:
-        conn = None
-        cur = None
-
-        try:
-            conn = psycopg2.connect(host = hostname, dbname = database, user = username, password = pwd, port = port_id)
-            print("connected")
-            cur = conn.cursor()
-
-            enterProfile_script = 'INSERT INTO users (fname, lname, age, gender) VALUES (%s, %s, %s, %s)'
-            user_values = (FirstName, LastName, Age, Gender)
-            cur.execute(enterProfile_script,user_values)
-
-
-            conn.commit()
-        except Exception as error:
-            print(error)
-        finally:
-            if cur is not None:
-                cur.close()
-            if conn is not None:
-                conn.close()
-               
-        ar_profile()#function fills array of profile database data
-        update_current_user(global_array[-1])#current_user hold tupal of created user
-        #current_user = global_array[-1]#current_user hold tupal of created user
-        
-    return redirect('/static/UmamindFoodInfo.html')
-
-
-@app.route('/profile', methods=['POST'])#function that executes on accesss to prfile page and searches for correct profile to display
-def access_profile():
-
-    if request.method == 'POST':
-        #print("test")
-        count = len(global_array)#obtain num of elements in array
-        print("current is: ")
-        print(current_user)
-        ID = current_user[0]
-        fn = current_user[1]
-        ls = current_user[2]
-        age = current_user[3]
-        gender = current_user[4]
-        print("test")
-        print(ID,fn,ls,age,gender)
-        
-        return render_template('UmamindProfile.html', f = fn, l = ls, a = age, g = gender)
-        
-        '''
-        for i in range(count):
-            data = global_array[i]
-            fn = data['fname']
-            if fn == current_user:
-                #print("test")
-                prof_info = global_array[i]
-                i = count + 1#end for loop
-                print("entered if state")
-                break
-            else:
-                print("In else state")
-                prof_info = "no_user"
-
-        print("fn is: ",fn)
-        print("prof_info is: ",prof_info)
-        if prof_info == "no_user":
-            return render_template('UmamindProfile.html', f = "no data found", l = "no data found")#variables f and l are sent and used in UmamindProfile.html
-        f_name = prof_info['fname']
-        l_name = prof_info['lname']
-
-    return render_template('UmamindProfile.html', f = f_name, l = l_name)#variables f and l are sent and used in UmamindProfile.html
-    '''
-
-
-
-#g is flask for a global var storage 
-def init_new_env():
-    #To connect to DB
-    if 'db' not in g:
-        g.db = get_db()
-
-    if 'hb' not in g:
-        g.hb = get_head_band_sensor_object()
-
-    #g.secrets = get_secrets()
-    #g.sms_client = get_sms_client()
-
-#This gets executed by default by the browser if no page is specified
-#So.. we redirect to the endpoint we want to load the base page
-@app.route('/') #endpoint
-def index():
-    return redirect('/static/UmamindData.html')
-
-
-@app.route("/secure_api/<proc_name>",methods=['GET', 'POST'])
-@token_required
-def exec_secure_proc(proc_name):
-    logger.debug(f"Secure Call to {proc_name}")
-
-    #setup the env
-    init_new_env()
-
-    #see if we can execute it..
-    resp = ""
+#This function will calculate the Eucledian scores for all user in the table based on the current user ID#.
+def calcEuclidean(currentID):
     try:
-        fn = getattr(__import__('secure_calls.'+proc_name), proc_name)
-        resp = fn.handle_request()
-    except Exception as err:
-        ex_data = str(Exception) + '\n'
-        ex_data = ex_data + str(err) + '\n'
-        ex_data = ex_data + traceback.format_exc()
-        logger.error(ex_data)
-        return json_response(status_=500 ,data=ERROR_MSG)
+        conn = psycopg2.connect(host=hostname, dbname=database, user=username, password=pwd, port=port_id)
+        print("connected")
+        cur = conn.cursor()
 
-    return resp
+        #get averages data of the current user
+        cur.execute(f"SELECT o1, o2, t3, t4 FROM data WHERE user_id = {currentID}")
+        current_user_data = cur.fetchone()
+        
 
+        if current_user_data:
+            cur_o1, cur_o2, cur_t3, cur_t4 = current_user_data #insert current user data into variables
 
+            #Fetch data for all users except the current one
+            cur.execute(f"SELECT user_id, o1, o2, t3, t4 FROM data WHERE user_id <> {currentID}")
+            other_users_data = cur.fetchall() #store it all in one variable
 
-@app.route("/open_api/<proc_name>",methods=['GET', 'POST'])
-def exec_proc(proc_name):
-    logger.debug(f"Call to {proc_name}")
+            #loop through other_users_data, run the euclidean equation, store it into the other user euclidean field
+            for other_user_id, other_o1, other_o2, other_t3, other_t4 in other_users_data:
+                euclideanVal = sqrt(
+                    (cur_o1 - other_o1)**2 +
+                    (cur_o2 - other_o2)**2 +
+                    (cur_t3 - other_t3)**2 +
+                    (cur_t4 - other_t4)**2
+                )
+                #query for inserting euclidean values
+                variables = (euclideanVal,other_user_id)
+                cur.execute('UPDATE data SET euclidean = %s WHERE user_id = %s', variables)
 
-    #setup the env
-    init_new_env()
+            variables = (None, currentID)
+            cur.execute('UPDATE data SET euclidean = %s WHERE user_id = %s', variables)
+                
+            
+            #end of for
+        #end of if
 
-    #see if we can execute it..
-    resp = ""
-    try:
-        fn = getattr(__import__('open_calls.'+proc_name), proc_name)
-        resp = fn.handle_request()
-    except Exception as err:
-        ex_data = str(Exception) + '\n'
-        ex_data = ex_data + str(err) + '\n'
-        ex_data = ex_data + traceback.format_exc()
-        logger.error(ex_data)
-        return json_response(status_=500 ,data=ERROR_MSG)
-
-    return resp
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
-
-    
+        conn.commit()
+    except Exception as error:
+        print(error)
+    finally: 
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()    
